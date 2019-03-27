6d28cededa26040bea97bb660d0aca306d4c84a2
Carl Hage <ch@carlhage.com>
2019-03-26 19:15:33 -0700

    Add scripts to generate June,Nov 2018 results

---
+++ generate-1811.sh
@@ -0,0 +1,9 @@
+set -x
+
+ORR_PATH=submodules/osv-results-reporter
+
+
+orr -v --input "submodules/osv-sample-data/2018-11-06/out-orr/" \
+    --extra "${ORR_PATH}/templates/test-minimal/extra" \
+    --template "${ORR_PATH}/templates/test-minimal" \
+    --output-parent docs --output-dir 2018-11-06
