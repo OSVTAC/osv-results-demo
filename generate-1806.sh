7d7ec6fa8d2f68185c437215cfc240ff7270e75c
Carl Hage <ch@carlhage.com>
2019-05-12 00:15:53 -0700

    Update script to getnerate 2018-06-05 results

--- generate-1806.sh
+++ generate-1806.sh
@@ -3,7 +3,14 @@ set -x
 ORR_PATH=submodules/osv-results-reporter
 
 
-orr -v --input-dir "submodules/osv-sample-data/2018-06-05/out-orr/" \
-    --extra "${ORR_PATH}/templates/test-minimal/extra" \
-    --template "${ORR_PATH}/templates/test-minimal" \
-    --output-parent docs --output-dir 2018-06-05
+orr -v --input-dir submodules/osv-sample-data/2018-06-05/out-orr/ \
+    --template-dir "${ORR_PATH}/templates/test-minimal" \
+    --extra-template-dirs "${ORR_PATH}/templates/test-minimal/extra" \
+    --output-parent docs --output-subdir 2018-06-05
+
+# orr-docker --input-dir submodules/osv-sample-data/2018-06-05/out-orr/ \
+#     --template-dir "${ORR_PATH}/templates/test-minimal" \
+#     --extra-template-dirs "${ORR_PATH}/templates/test-minimal/extra" \
+#     --output-parent docs --output-subdir 2018-06-05 \
+#     --source-dir submodules/osv-results-reporter \
+#     --orr -v
