4472fc57c17e487a8ba3971d0d1ec5489cfb3725
Carl Hage <ch@carlhage.com>
2019-05-28 14:58:19 -0700

    Regenerate after header definitions were fixed

--- generate-1811-zero.sh
+++ generate-1811-zero.sh
@@ -1,12 +1,11 @@
 # Generate "zero reports" for the Nov. 2018 election.
-
 set -x
 
 ORR_PATH=submodules/osv-results-reporter
 
 
-orr -v --input-dir "submodules/osv-sample-data/2018-11-06/out-orr/" \
+orr -v --input-dir submodules/osv-sample-data/2018-11-06/out-orr/ \
     --input-results-dir "submodules/osv-sample-data/2018-11-06/out-orr/resultdata-zero/" \
-    --extra "${ORR_PATH}/templates/test-minimal/extra" \
-    --template "${ORR_PATH}/templates/test-minimal" \
-    --output-parent docs --output-dir 2018-11-06-zero
+    --template-dir "${ORR_PATH}/templates/test-minimal" \
+    --extra-template-dirs "${ORR_PATH}/templates/test-minimal/extra" \
+    --output-parent docs --output-subdir 2018-11-06-zero
